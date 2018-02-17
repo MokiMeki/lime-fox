bf74d6ad0cb4451889b286bf839868034b77fb9e
MokiMeki <snow_hotel@hotmail.com>
2018-02-18 01:40:38 +0700

    more soft soft touch

--- lime-fox/softtouch.cpp
+++ lime-fox/softtouch.cpp
@@ -5,8 +5,11 @@
 #define IDLE_G 200
 #define IDLE_B 255
 #define PRESSED_R 255
-#define PRESSED_G 0
-#define PRESSED_B 0
+#define PRESSED_G 50
+#define PRESSED_B 50
+#define HALF_PRESSED_R (IDLE_R+PRESSED_R)/2
+#define HALF_PRESSED_G (IDLE_R+PRESSED_G)/2
+#define HALF_PRESSED_B (IDLE_R+PRESSED_B)/2
 
 static KEYS_MATRIX KeyMatrix = KEYS_MATRIX();
 static COLOR_MATRIX ColorMatrix = COLOR_MATRIX();
@@ -17,6 +20,10 @@ void key_press_callback(int iRow, int iColumn, bool bPressed) {
 		int afterColumn = limefox_limit(iColumn + 1, 0, S_MAX_LED_COLUMN);
 		int beforeRow = limefox_limit(iRow - 1, 0, S_MAX_LED_ROW);
 		int afterRow = limefox_limit(iRow + 1, 0, S_MAX_LED_ROW);
+		int beforeColumn2 = limefox_limit(iColumn - 2, 0, S_MAX_LED_COLUMN);
+		int afterColumn2 = limefox_limit(iColumn + 2, 0, S_MAX_LED_COLUMN);
+		int beforeRow2 = limefox_limit(iRow - 2, 0, S_MAX_LED_ROW);
+		int afterRow2 = limefox_limit(iRow + 2, 0, S_MAX_LED_ROW);
 
 		limefox_setKeyColor(&ColorMatrix, iRow, iColumn, PRESSED_R, PRESSED_G, PRESSED_B);
 		limefox_setKeyColor(&ColorMatrix, iRow, beforeColumn, PRESSED_R, PRESSED_G, PRESSED_B);
@@ -27,6 +34,11 @@ void key_press_callback(int iRow, int iColumn, bool bPressed) {
 		limefox_setKeyColor(&ColorMatrix, afterRow, iColumn, PRESSED_R, PRESSED_G, PRESSED_B);
 		limefox_setKeyColor(&ColorMatrix, afterRow, beforeColumn, PRESSED_R, PRESSED_G, PRESSED_B);
 		limefox_setKeyColor(&ColorMatrix, afterRow, afterColumn, PRESSED_R, PRESSED_G, PRESSED_B);
+
+		limefox_setKeyColor(&ColorMatrix, iRow, beforeColumn2, HALF_PRESSED_R, HALF_PRESSED_G, HALF_PRESSED_B);
+		limefox_setKeyColor(&ColorMatrix, iRow, afterColumn2, HALF_PRESSED_R, HALF_PRESSED_G, HALF_PRESSED_B);
+		limefox_setKeyColor(&ColorMatrix, beforeRow2, iColumn, HALF_PRESSED_R, HALF_PRESSED_G, HALF_PRESSED_B);
+		limefox_setKeyColor(&ColorMatrix, afterRow2, iColumn, HALF_PRESSED_R, HALF_PRESSED_G, HALF_PRESSED_B);
 	}	
 	KeyMatrix.Keys[iRow][iColumn].isPressed = bPressed;
 }
